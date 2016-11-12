ae162cdda4b78a813cc1cdfebb09492361e88a00
김준현 <tacan99@hotmail.com>
2016-11-12 18:59:15 +0900

    init commit

---
+++ Assets/02.Scripts/EffectMgr.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectMgr : MonoBehaviour {
+    public static EffectMgr instance;
+    public GameObject stone;
+    public Transform stonePosition;
+
+    void Awake() {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        stonePosition = GameObject.Find("StonePosition").transform;
+        stone = Resources.Load<GameObject>("Prefabs/stone");
+        StartCoroutine(this.CreateStone());
+	}
+	
+    IEnumerator CreateStone() {
+        while(true) {
+            GameObject _stone = (GameObject)Instantiate(stone, stonePosition.position, Quaternion.identity);
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
+}
---
+++ Assets/02.Scripts/StoneCtrl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneCtrl : MonoBehaviour {
+    private Transform tr;
+    private Transform targetTr;
+    public float speed = 20.0f;
+	// Use this for initialization
+	void Start () {
+        tr = GetComponent<Transform>();
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("TARGET");
+        targetTr = gameObjects[Random.Range(0, gameObjects.Length)].transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Quaternion rot = Quaternion.LookRotation(targetTr.position, tr.position);
+        tr.LookAt(targetTr);
+        tr.Translate(Vector3.forward * Time.deltaTime * speed);
+	}
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.CompareTag("TARGET"))
+        {
+            Destroy(this.gameObject);
+            Debug.Log("stone");
+        }
+    }
+}
